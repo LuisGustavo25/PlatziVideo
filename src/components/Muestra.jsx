bdccbe22005018d3e751b2f4657fb0f6f28c9ff0
LuisGustavo <acostarluisg@gmail.com>
2021-03-27 21:53:03 +0100

    configuration

---
+++ src/components/Muestra.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const Muestra = () =>{
+    return(<h1>hola</h1>);
+    
+}
+
+export default Muestra;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Muestra from './components/Muestra';
+
+ReactDOM.render(<Muestra></Muestra>, document.getElementById('app'));
+
+
